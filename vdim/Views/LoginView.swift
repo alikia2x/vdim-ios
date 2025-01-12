a434ac3403c0844f6df3d23aebb167339812f925
alikia2x <alikia2x@outlook.com>
2025-01-12 23:27:29 +0800

    add: LoginView 登录/注册切换

--- vdim/Views/LoginView.swift
+++ vdim/Views/LoginView.swift
@@ -1,5 +1,10 @@
 import SwiftUI
 
+enum AuthState {
+    case login
+    case register
+}
+
 private struct GradientTitle: View {
     var text: String
     var fontSize: CGFloat
@@ -11,37 +16,79 @@ private struct GradientTitle: View {
             .background(
                 LinearGradient(
                     gradient: Gradient(colors: [
-                        Color(red: 216 / 255, green: 136 / 255, blue: 242 / 255),
-                        Color(red: 200 / 255, green: 141 / 255, blue: 255 / 255),
-                        Color(red: 136 / 255, green: 165 / 255, blue: 254 / 255),
-                        Color(red: 102 / 255, green: 204 / 255, blue: 255 / 255),
+                        Color(
+                            red: 216 / 255, green: 136 / 255, blue: 242 / 255),
+                        Color(
+                            red: 200 / 255, green: 141 / 255, blue: 255 / 255),
+                        Color(
+                            red: 136 / 255, green: 165 / 255, blue: 254 / 255),
+                        Color(
+                            red: 102 / 255, green: 204 / 255, blue: 255 / 255),
                     ]),
                     startPoint: .top,
                     endPoint: .bottomTrailing
                 )
-                .mask(Text(text).font(Font.custom("Alimama ShuHeiTi Bold", size: fontSize)))
+                .mask(
+                    Text(text).font(
+                        Font.custom("Alimama ShuHeiTi Bold", size: fontSize)))
             )
     }
 }
 
-
-
 private struct Title: View {
     var body: some View {
         VStack(alignment: .leading, spacing: -5) {
-            Glow() {
+            Glow {
                 Text("欢迎来到")
                     .font(.system(size: 32, weight: .bold))
                     .foregroundColor(.white)
             }
-            Glow() {
+            Glow {
                 GradientTitle(text: "V次元", fontSize: 52)
             }
         }
     }
 }
 
+private struct LoginRegisterSwitcher: View {
+    @Binding var currentState: AuthState
+
+    var body: some View {
+        HStack(alignment: .bottom) {
+            Text("登录")
+                .font(
+                    currentState == .login
+                        ? .system(size: 32, weight: .bold)
+                        : .system(size: 26, weight: .medium)
+                )
+                .foregroundColor(
+                    currentState == .login ? .blue : Color(hex: "#F8FBFF")
+                )
+                .onTapGesture {
+                    currentState = .login
+                }
+
+            Text("注册")
+                .font(
+                    currentState == .register
+                        ? .system(size: 32, weight: .bold)
+                        : .system(size: 26, weight: .medium)
+                )
+                .foregroundColor(
+                    currentState == .register ? .blue : Color(hex: "#F8FBFF")
+                )
+                .onTapGesture {
+                    currentState = .register
+                }
+            Spacer()
+        }
+        .padding(.leading, 12)
+    }
+}
+
 struct LoginView: View {
+    @State private var currentState: AuthState = .login
+
     var body: some View {
         ZStack {
             Color.black
@@ -56,18 +103,53 @@ struct LoginView: View {
 
             VStack(alignment: .leading, spacing: 8) {
                 Title()
+                // 为什么这里的alignment不生效
+                VStack(alignment: .leading) {
+                    LoginRegisterSwitcher(currentState: $currentState)
+                        .padding(.top, 24)
+                        .padding(.leading, 12)
+                    Spacer()
+                }
+                .frame(width: UIScreen.main.bounds.width - 48, height: 480)
+                .background(
+                    .thinMaterial, in: RoundedRectangle(cornerRadius: 13)
+                )
+                .padding(.top, 16)
+
             }
-            .frame(maxWidth: UIScreen.main.bounds.width,
-                   maxHeight: UIScreen.main.bounds.height, alignment: .topLeading)
-            .padding(.top, 300)
+            .frame(
+                maxWidth: UIScreen.main.bounds.width,
+                maxHeight: UIScreen.main.bounds.height, alignment: .topLeading
+            )
+            .padding(.top, 250)
             .padding(.leading, 48)
         }
     }
 }
 
-
 struct Login_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+    }
+}
+
+extension Color {
+    init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized.remove(at: hexSanitized.startIndex)
+        }
+
+        var rgb: UInt64 = 0
+        let scanner = Scanner(string: hexSanitized)
+        guard scanner.scanHexInt64(&rgb) else {
+            return nil  // 如果扫描失败，返回 nil
+        }
+
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >> 8) & 0xFF) / 255.0
+        let b = Double(rgb & 0xFF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
     }
 }
